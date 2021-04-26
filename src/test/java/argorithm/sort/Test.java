10d556e5867adcf13520497d1d9144bedb8044db
chuanskqi <shenkang777@163.com>
2021-04-26 23:25:25 +0800

    冒泡排序优化

--- src/test/java/argorithm/sort/Test.java
+++ src/test/java/argorithm/sort/Test.java
@@ -16,6 +16,10 @@ public class Test {
      * @param n
      */
     public static void bubbleSort(int[] a, int n) {
+        if (n <= 1) {
+            return ;
+        }
+
         //i 表示什么 i表示需要进行冒泡的次数。即只需要n-1次循环比较就可以排序
         //0~n-2次，共计 n-1次比较，每一次比较将最大的值摘出来
 
@@ -23,15 +27,23 @@ public class Test {
         //j表示数组的下标，每轮比较，从数组0 1 2 ...
         for (int i = 0; i < n - 1; i++) {
             System.out.println("第" + (i + 1) + "次外层循环开始:" + Arrays.toString(a));
+
+            boolean switchFlag = false;
             for (int j = 0; j < n - 1 - j; j ++) {
                 System.out.println("进入处理");
                 if (a[j] > a[j+1]) {
                     int temp = a[j];
                     a[j] = a[j+1];
                     a[j+1] = temp;
+                    switchFlag = true;
                 }
             }
             System.out.println("第" + (i + 1) + "次外层循环结束:" + Arrays.toString(a));
+
+            if (!switchFlag)  {
+                //当一次比较没有交换。证明已经排序完毕
+                break;
+            }
         }
     }
 }
